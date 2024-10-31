24d37103e26be715c70badd9c506b9503ba95bb1
Diego Luis Mayolo <diego.mayolo@outlook.com>
2024-10-30 21:22:28 -0300

    fix: cors configuration settings

--- src/main/java/com/loginauthapi/infra/cors/CorsConfig.java
+++ src/main/java/com/loginauthapi/infra/cors/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig implements WebMvcConfigurer
     public void addCorsMappings( CorsRegistry registry) 
     {
         registry.addMapping( "/**" )
-                .allowedHeaders("*")
                 .allowedOrigins( "http://localhost:4200" )
                 .allowedMethods( "GET", "POST", "DELETE", "PUT" );
     }
